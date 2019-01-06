47a10daf8086a2bb1605f385166dd12b889f7714
Mike.McLean <Mike.McLean@softwire.com>
2019-01-06 10:23:55 +0000

    Cleanup so that DbContext is buildable

--- Budgetting/DbModels/BudgettingDbContext.cs
+++ Budgetting/DbModels/BudgettingDbContext.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Budgetting.DbModels.BankAccounts;
+using Budgetting.DbModels.BudgetAllowances;
+using Budgetting.DbModels.Budgets;
 using Budgetting.DbModels.Helpers;
+using Budgetting.DbModels.Predictors;
 using Microsoft.EntityFrameworkCore;
 
 namespace Budgetting.DbModels
@@ -13,6 +17,26 @@ namespace Budgetting.DbModels
       : base(options)
     {
     }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<AccountTransaction> AccountTransactions { get; set; }
+    public DbSet<RefundTransaction> RefundTransaction { get; set; }
+    public DbSet<TransferTransaction> TransferTransaction { get; set; }
+
+    public DbSet<Budget> Budgets { get; set; }
+    public DbSet<BudgetCategory> BudgetCategories { get; set; }
+    public DbSet<BudgetMonth> BudgetMonths { get; set; }
+    public DbSet<BudgetTransaction> BudgetTransactions { get; set; }
+
+    public DbSet<DiscretionaryBudgetTransfer> BudgetTransfers { get; set; }
+    public DbSet<TemporalBudgetShift> TemporalBudgetShifts { get; set; }
+    public DbSet<WindfallBudgetDivision> WindfallDivisions { get; set; }
+
+    public DbSet<TransactionPredictor> TransactionPredictors { get; set; }
+    public DbSet<SplittableTransactionPredictor> SplittableTransactionPredictors { get; set; }
+    public DbSet<TransactionBudgetPredictor> TransactionBudgetPredictors { get; set; }
+    public DbSet<PartialTransferPredictor> PartialTransferPredictors { get; set; }
+    public DbSet<TransferPairPredictor> TransferPairPredictors { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
