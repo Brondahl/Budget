6f45b6697f9276e093f5fd3715bca92c510fb029
Mike.McLean <Mike.McLean@softwire.com>
2019-01-28 20:08:24 +0000

    Begin TSB drawdown

---
+++ Budgetting/Services/Downloaders/TSB/TsbDownloader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Budgetting.Services.Downloaders.TSB
+{
+  public class TsbDownloader : WebPortalTransactionDownloader<TsbCredentials, TsbPortalDriver>
+  {
+    protected override IEnumerable<SingleAccountDownloader<TsbPortalDriver>> InternalDownloaders
+    {
+      get
+      {
+        return;
+      }
+    }
+
+    protected override TsbCredentials LoadSpecificCredentials(IAccountCredentials credentials)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Budgetting/Services/Downloaders/TSB/TsbPortalDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Budgetting.Services.Downloaders.TSB
+{
+  public class TsbPortalDriver : IWebPortalDriver<TsbCredentials>
+  {
+    public void GoToHomePage()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Login(TsbCredentials credentials)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
