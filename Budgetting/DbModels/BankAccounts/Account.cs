5af02377231736dfb06032623896c813e5e5ee5f
Mike.McLean <Mike.McLean@softwire.com>
2019-01-29 21:41:56 +0000

    Define KnownAccounts

--- Budgetting/DbModels/BankAccounts/Account.cs
+++ Budgetting/DbModels/BankAccounts/Account.cs
@@ -13,6 +13,48 @@ namespace Budgetting.DbModels.BankAccounts
     Other
   }
 
+  public static class KnownAccounts
+  {
+    public static Account MikeTsbCurrentAccount;
+    public static Account MikeTsbCreditCard;
+    public static Account JointTsbCurrentAccount;
+    public static Account CharlotteTsbCurrentAccount;
+    public static Account CharlotteTsbCreditCard;
+    public static Account TsbMortgage;
+
+    public static Account MikeNationwideCurrentAccount;
+    public static Account MikeNationwideCreditCard;
+
+    public static Account CharlotteSantanderCurrentAccount;
+    public static Account MikeSantanderCurrentAccount;
+    public static Account MikeSantanderCreditCard;
+
+    public static Account Sbcw;
+
+    public static Account ManualEntry;
+
+    public static void Populate()
+    {
+      MikeTsbCurrentAccount = new Account();
+      MikeTsbCreditCard = new Account();
+      JointTsbCurrentAccount = new Account();
+      CharlotteTsbCurrentAccount = new Account();
+      CharlotteTsbCreditCard = new Account();
+      TsbMortgage = new Account();
+     
+      MikeNationwideCurrentAccount = new Account();
+      MikeNationwideCreditCard = new Account();
+     
+      CharlotteSantanderCurrentAccount = new Account();
+      MikeSantanderCurrentAccount = new Account();
+      MikeSantanderCreditCard = new Account();
+     
+      Sbcw = new Account();
+     
+      ManualEntry = new Account();
+    }
+  }
+
   public class Account
   {
     public int Id { get; set; }
